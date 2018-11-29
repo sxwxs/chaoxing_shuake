32037dd93dac8fb38d7554b388b19e9309fc5a7c
sxw <sxwxs@msn.com>
2018-11-29 12:11:36 +0800

    init

---
+++ main.py
@@ -0,0 +1,69 @@
+from selenium import webdriver, common
+import time
+from utils import play_loop, get_cur_title
+
+#def main():
+if True:
+    prefs = {
+        "profile.default_content_setting_values.plugins": 1,
+        "profile.content_settings.plugin_whitelist.adobe-flash-player": 1,
+        "profile.content_settings.exceptions.plugins.*,*.per_resource.adobe-flash-player": 1,
+        "PluginsAllowedForUrls": "https://chaoxing.com"
+    }
+    options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')
+    # options.add_argument('--disable-gpu')
+    options.add_experimental_option("prefs", prefs)
+    driver = webdriver.Chrome(chrome_options=options)
+    raw_cookie = ''
+    try:
+        driver.set_window_size(1920, 1080)
+    except common.exceptions.WebDriverException:
+        pass
+    driver.get('http://passport2.chaoxing.com/login?fid=22169&refer=http://i.mooc.chaoxing.com')
+    print ('========================')
+    print ('请手动登陆，进入学习页面，然后按回车继续，窗口可能会自动切换')
+    print ('========================')
+    input("")
+    # change to course window
+    for x in driver.window_handles:
+        driver.switch_to.window(x)
+        if driver.title == '学习进度页面':
+            break
+    driver.switch_to.default_content()
+    time.sleep(2)
+    # get course list and print it
+    lsx = driver.find_elements_by_class_name('ncells')
+
+    time.sleep(1)
+    i = 0
+    
+    try:
+        for l in lsx:
+            print ('[', i, ']', l.text)
+            #time.sleep(0.1)
+            i += 1
+        i = int(input('请输入接下来需要观看的课程编号（方括号内的数字）:'))
+    except selenium.common.exceptions.StaleElementReferenceException:
+        print ('错误，无法打印课程列表')
+        while True:
+            i = int(input('请输入0-%d的数字我们将跳到那个课程的页面，你可以确认那是否是你要观看的课程:'%len(lsx)-1))
+            lsx = driver.find_elements_by_class_name('ncells')
+            lsx[i].click()
+            while True:
+                ch = input('从当前课程开始播放?(y/N):')
+                if ch == 'y':
+                    break
+            if ch == 'y':
+                break
+    while i < len(lsx):
+        driver.switch_to.default_content()
+        lsx = driver.find_elements_by_class_name('ncells')
+        # go to current course page
+        lsx[i].click()
+        time.sleep(3)
+        # play video and answer question in video
+        play_loop(driver)
+        print (get_cur_title(driver), '结束')
+        i += 1
+    print ('全部视频结束')
---
+++ utils.py
@@ -0,0 +1,130 @@
+import os
+import time
+import socket
+import selenium
+import json
+import math
+
+def switch_to_video_frame(driver):
+    driver.switch_to.frame(driver.find_elements_by_tag_name('iframe')[0])
+    driver.switch_to.frame(driver.find_elements_by_tag_name('iframe')[0])
+
+def start_play(driver):
+    switch_to_video_frame(driver)
+    # click play button in the center of the screen
+    driver.find_element_by_xpath('//*[@id="video"]/button').click()
+    driver.switch_to.default_content()
+
+def reseum(driver):
+    switch_to_video_frame(driver)
+    # click play button in the control panel
+    driver.find_element_by_xpath('//*[@id="video"]/div[4]/button[1]').click()
+    driver.switch_to.default_content()
+
+def recv_until_nl(s):
+    # recive from socket byte by byte until \n
+    text = b''
+    while True:
+        ch = ''
+        while len(ch) == 0:
+                ch = s.recv(1)
+        if ch == b'\n':
+                return text.decode('utf8')
+        text += ch
+                
+def ans_question(driver):
+    switch_to_video_frame(driver)
+    # get question title element
+    question_title_element = driver.find_element_by_xpath('//div[@class="ans-videoquiz-title"]')
+    question = question_title_element.text
+    print(question)
+    options = []
+    uls = question_title_element.parent.find_elements_by_tag_name('ul')
+    for ul in uls:
+        # find options list
+        if 'ans-videoquiz-opts' in ul.get_attribute('class'):
+            break
+    if 'ans-videoquiz-opts' not in ul.get_attribute('class'):
+        raise Exception('error can not find options')
+    lis = ul.find_elements_by_tag_name('li')
+    for li in lis:
+        options.append(li.text)
+    s = socket.socket()
+    s.connect(('as.codeplot.top', 1507))
+    s.send((question.replace('\n', '\\n')+'\n').encode('utf8'))
+    ans = int(recv_until_nl(s))
+    s.close()
+    if ans == -1: # -1 
+        ans = 0
+        while ans < len(lis):
+            lis[ans].find_element_by_tag_name('label').click()
+            driver.find_elements_by_class_name('ans-videoquiz-submit')[0].click()
+            time.sleep(1)
+            try:
+                alertx = driver.switch_to.alert
+                alertx.accept()
+                # wrong ans
+            except selenium.common.exceptions.NoAlertPresentException:
+                s = socket.socket()
+                s.connect(('as.codeplot.top', 1507))
+                s.send((json.dumps({'question': question.replace('\n', '\\n'), 'options': options, 'ans': ans}) + '\n').encode('utf8'))
+                s.close()
+                break
+            ans += 1
+    else:
+        lis[ans].find_element_by_tag_name('label').click()
+        driver.find_elements_by_class_name('ans-videoquiz-submit')[0].click()
+    f = open('question.jsonl', 'a')
+    f.write(json.dumps({'question': question, 'options': options, 'ans': ans})+'\n')
+    f.close()
+    driver.switch_to.default_content()
+    #alert.text == '回答有错误'
+    #alert.accept()
+
+def get_video_duration(driver):
+    switch_to_video_frame(driver)
+    vd = driver.find_elements_by_xpath('//*[@id="video_html5_api"]')
+    duration = vd[0].get_attribute('duration')
+    driver.switch_to.default_content()
+    return duration
+
+
+def get_video_curTime(driver):
+    switch_to_video_frame(driver)
+    vd = driver.find_elements_by_xpath('//*[@id="video_html5_api"]')
+    curTime = vd[0].get_attribute('currentTime')
+    driver.switch_to.default_content()
+    return curTime
+
+def play_loop(driver):
+    start_play(driver)
+    time.sleep(2)
+    cur_duration = float(get_video_duration(driver))
+    print (cur_duration / 60, 'min')
+    flag = False
+    last_time = 0
+    while True:
+        if math.isnan(cur_duration):
+            cur_duration = float(get_video_duration(driver))
+        time.sleep(10)
+        cur_time = float(get_video_curTime(driver))
+        if cur_time + 1 >= cur_duration:
+            return
+        if cur_time > last_time:
+            flag = False
+            last_time = cur_time
+            lt = time.localtime()
+            print ('%d-%d-%d %d:%d --  %f %%'%(lt.tm_year, lt.tm_mon, lt.tm_mday, lt.tm_hour, lt.tm_min, cur_time*100.0 / cur_duration))
+        else:
+            if flag:
+                ans_question(driver)
+            else:
+                try:
+                    reseum(driver)
+                except selenium.common.exceptions.WebDriverException:
+                    driver.switch_to.default_content()
+                flag = True
+
+def get_cur_title(driver):
+    driver.switch_to.default_content()
+    return driver.find_element_by_xpath('//*[@id="mainid"]/h1').text
